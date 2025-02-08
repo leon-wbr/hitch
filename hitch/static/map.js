e43467a790332d7b1b92d56b58264de4f484998a
tillwenke <till.wenke@gmx.net>
2025-02-08 12:29:54 +0100

    fix: trip filter

--- hitch/static/map.js
+++ hitch/static/map.js
@@ -259,6 +259,9 @@ function setupFilterEventListeners() {
   distanceFilter.addEventListener("input", () =>
     setQueryParameter("mindistance", distanceFilter.value)
   );
+  tripFilter.addEventListener("input", () =>
+    setQueryParameter("trip", tripFilter.value)
+  );
 }
 
 // Update the direction query parameter based on knob rotation
@@ -674,6 +677,7 @@ const knobToggle = document.getElementById("knob-toggle");
 const textFilter = document.getElementById("text-filter");
 const userFilter = document.getElementById("user-filter");
 const distanceFilter = document.getElementById("distance-filter");
+const tripFilter = document.getElementById("trip-filter");
 const clearFilters = document.getElementById("clear-filters");
 
 let isDragging = false,
@@ -751,12 +755,14 @@ function applyParams() {
   textFilter.value = getQueryParameter("text");
   userFilter.value = getQueryParameter("user");
   distanceFilter.value = getQueryParameter("mindistance");
+  tripFilter.value = getQueryParameter("trip");
 
   if (
     knobToggle.checked ||
     textFilter.value ||
     userFilter.value ||
-    distanceFilter.value
+    distanceFilter.value ||
+    tripFilter.value
   ) {
     if (filterMarkerGroup) filterMarkerGroup.remove();
     if (filterDestLineGroup) filterDestLineGroup.remove();
@@ -797,6 +803,11 @@ function applyParams() {
             return true;
         }
         return false;
+      });
+    }
+    if (tripFilter.value) {
+      filterMarkers = filterMarkers.filter((x) => {
+        return x.options._row[5] == tripFilter.value;
       });
     }
     if (knobToggle.checked) {
